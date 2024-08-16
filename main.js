5fcfd7ac319baffc5d7c882078230d48e0363f6c
Huy <phanchihuy0310@gmail.com>
2024-08-16 11:45:43 +0700

    Initial commit

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var a;
+var x;
 // Lấy giá trị từ người dùng
 x = Number(prompt("Nhập vào số bất kỳ", "0"));
 // Kiểm tra số chẵn và lẽ
-if (a % 2 == 0) {
+if (x % 2 == 0) {
     document.write("x là số chẵn");
 } else {
     document.write("x là số lẻ");
